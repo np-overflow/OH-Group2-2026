7318f39104e622885e020cb7fcd9e65f248646f8
Sairam Suresh <sairam278.suresh@gmail.com>
2026-01-04 10:05:44 +0000

    Fix button not turning grey on loading

--- frontend/components/new/circular-continue-button.tsx
+++ frontend/components/new/circular-continue-button.tsx
@@ -8,14 +8,14 @@ export default function CircularContinueButton({ text = "Proceed", onClick, rest
     const [loading, setLoading] = useState(false);
 
     return (
-        <div className={`flex flex-col items-center gap-2 ${onClick || !loading ? "cursor-pointer" : ""}`} onClick={() => {
+        <div className={`flex flex-col items-center gap-2 ${onClick && !loading ? "cursor-pointer" : ""}`} onClick={() => {
             setLoading(true);
             onClick && onClick();
         }}>
-            <div className={`p-4 size-20  ${onClick || !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} rounded-full flex flex-row items-center justify-center`}>
+            <div className={`p-4 size-20  ${onClick && !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} rounded-full flex flex-row items-center justify-center`}>
                 {loading ? <Spinner /> : restart ? <RefreshCcw className="size-10 text-white" /> : <ArrowRight className="size-10 text-white" />}
             </div>
-            <div className={`${onClick || !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} text-white py-2 px-4 rounded-lg`}>
+            <div className={`${onClick && !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} text-white py-2 px-4 rounded-lg`}>
                 <p className="font-bold">{text}</p>
             </div>
         </div>
