61e3cf23e21144a25609355e71f221788a0e04b9
Sairam Suresh <sairam278.suresh@gmail.com>
2026-01-04 10:04:31 +0000

    Make the forward button turn grey when loading and restore old spinner

--- frontend/components/new/back-continue-button-container.tsx
+++ frontend/components/new/back-continue-button-container.tsx
@@ -1,12 +1,12 @@
 import CircularBackButton from "./circular-back-button";
 import CircularContinueButton from "./circular-continue-button";
 
-export default function BackContinueButtonContainer({ backText, continueText, onBack, onContinue, children }: { backText?: string, continueText?: string, onBack?: () => void, onContinue?: () => void, children?: React.ReactNode }) {
+export default function BackContinueButtonContainer({ backText, continueText, onBack, onContinue, children, restart = false }: { backText?: string, continueText?: string, onBack?: () => void, onContinue?: () => void, children?: React.ReactNode, restart?: boolean }) {
     return (
         <div className={`w-full flex flex-row ${!(onBack === undefined && onContinue === undefined) ? "justify-between" : "justify-center"} items-center px-52 py-4`}>
             {!(onBack === undefined && onContinue === undefined) && <CircularBackButton text={backText} onClick={onBack} />}
                 {children}
-            {!(onBack === undefined && onContinue === undefined) &&<CircularContinueButton text={continueText} onClick={onContinue} />}
+            {!(onBack === undefined && onContinue === undefined) &&<CircularContinueButton text={continueText} onClick={onContinue} restart={restart} />}
         </div>
     )
 }
--- frontend/components/new/circular-continue-button.tsx
+++ frontend/components/new/circular-continue-button.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, RefreshCcw } from "lucide-react";
 import { useState } from "react";
 import { Spinner } from "../Spinner";
 
-export default function CircularContinueButton({ text = "Proceed", onClick }: { text?: string, onClick?: () => void }) {
+export default function CircularContinueButton({ text = "Proceed", onClick, restart = false }: { text?: string, onClick?: () => void, restart?: boolean }) {
     const [loading, setLoading] = useState(false);
 
     return (
-        <div className={`flex flex-col items-center gap-2 ${onClick ? "cursor-pointer" : ""}`} onClick={() => {
+        <div className={`flex flex-col items-center gap-2 ${onClick || !loading ? "cursor-pointer" : ""}`} onClick={() => {
             setLoading(true);
             onClick && onClick();
         }}>
-            <div className={`p-4 size-20  ${onClick ? "bg-[#2C7AFC]" : "bg-gray-400"} rounded-full flex flex-row items-center justify-center`}>
-                {loading ? <Spinner /> : <ArrowRight className="size-10 text-white" />}
+            <div className={`p-4 size-20  ${onClick || !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} rounded-full flex flex-row items-center justify-center`}>
+                {loading ? <Spinner /> : restart ? <RefreshCcw className="size-10 text-white" /> : <ArrowRight className="size-10 text-white" />}
             </div>
-            <div className={`${onClick ? "bg-[#2C7AFC]" : "bg-gray-400"} text-white py-2 px-4 rounded-lg`}>
+            <div className={`${onClick || !loading ? "bg-[#2C7AFC]" : "bg-gray-400"} text-white py-2 px-4 rounded-lg`}>
                 <p className="font-bold">{text}</p>
             </div>
         </div>
